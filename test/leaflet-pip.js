d3df04e91c3885cc5544e3fefd7d5f24d413608c
Tom MacWright <tom@macwright.org>
2013-03-28 10:13:40 -0400

    Test exception, prune ies

--- test/leaflet-pip.js
+++ test/leaflet-pip.js
@@ -15,15 +15,22 @@ var gj = {
 };
 
 describe('Leaflet Point in Polygon', function() {
-  describe('simple polygon', function() {
-      it('should find a point in a layer', function() {
-          var gjLayer = L.geoJson(gj), poly;
-          gjLayer.eachLayer(function(l) { poly = l; });
-          expect(leafletPip.pointInLayer([50, 50], gjLayer)).to.eql([poly]);
-      });
-      it('should not find a point outside', function() {
-          var gjLayer = L.geoJson(gj), poly;
-          expect(leafletPip.pointInLayer([50, 150], gjLayer)).to.eql([]);
-      });
-  });
+    describe('simple polygon', function() {
+        it('should find a point in a layer', function() {
+            var gjLayer = L.geoJson(gj), poly;
+            gjLayer.eachLayer(function(l) { poly = l; });
+            expect(leafletPip.pointInLayer([50, 50], gjLayer)).to.eql([poly]);
+        });
+        it('should not find a point outside', function() {
+            var gjLayer = L.geoJson(gj), poly;
+            expect(leafletPip.pointInLayer([50, 150], gjLayer)).to.eql([]);
+        });
+    });
+    describe('error handling', function() {
+        it('should throw an error if the argument is not right', function() {
+            expect(function() {
+                leafletPip.pointInLayer([50, 150], {});
+            }).to.throwException(/Error must be L\.GeoJSON/);
+        });
+    });
 });
