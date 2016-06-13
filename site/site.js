d6232f2c059bb6c6caf90aac4c3311c337599bd8
bsudekum <bobby@mapbox.com>
2016-06-13 15:21:54 -0700

    use https

--- site/site.js
+++ site/site.js
@@ -2,7 +2,7 @@ var leafletPip = require('../'),
     map = L.map('map').setView([37.8, -96], 4),
     gjLayer = L.geoJson(statesData);
 
-L.tileLayer('http://a.tiles.mapbox.com/v3/tmcw.map-l1m85h7s/{z}/{x}/{y}.png')
+L.tileLayer('https://a.tiles.mapbox.com/v3/tmcw.map-l1m85h7s/{z}/{x}/{y}.png')
     .addTo(map);
 
 gjLayer.addTo(map);
